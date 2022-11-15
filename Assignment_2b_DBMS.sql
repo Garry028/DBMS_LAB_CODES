fbdaa6aeae7e534fdb49025eaa96ccc72ecd9d1e
Garry <gmisal2002@gmail.com>
2022-11-15 15:19:09 +0530

    changes

--- Assignment_2b_DBMS.sql
+++ Assignment_2b_DBMS.sql
@@ -1,31 +1,31 @@
-CREATE TABLE student_info (
-id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Stu_info (
+id INT PRIMARY KEY,
 rollno INT NOT NULL,
 name varchar(255),
 email varchar(255)
 );
 
-INSERT INTO student_info (rollno,name,email) VALUES ('81','mihir','mihir@gmail.com');
-INSERT INTO student_info (rollno,name,email) VALUES ('82','megha','megha@gmail.com');
-INSERT INTO student_info (rollno,name,email) VALUES ('22','Garry','garry@gmail.com');
-INSERT INTO student_info (rollno,name,email) VALUES ('98','madhur','madhur@gmail.com');
-INSERT INTO student_info (rollno,name,email) VALUES ('95','mohit','mohit@gmail.com');
+INSERT INTO Stu_info (id,rollno,name,email) VALUES ('1','81','mihir','mihir@gmail.com');
+INSERT INTO Stu_info (id,rollno,name,email) VALUES ('2','82','megha','megha@gmail.com');
+INSERT INTO Stu_info (id,rollno,name,email) VALUES ('3','22','Garry','garry@gmail.com');
+INSERT INTO Stu_info (id,rollno,name,email) VALUES ('4','98','madhur','madhur@gmail.com');
+INSERT INTO Stu_info (id,rollno,name,email) VALUES ('5','95','mohit','mohit@gmail.com');
 
-SELECT * FROM student_info;
-SELECT name FROM student_info WHERE rollno="82"
-SELECT * FROM student_info WHERE email="garry@gmail.com" AND  rollno="22"
-
-
-desc student_info;
+SELECT * FROM Stu_info ORDER BY id ASC;
+SELECT name,email FROM Stu_info WHERE rollno='82';
+SELECT * FROM Stu_info WHERE email='garry@gmail.com' AND  rollno='22'
 
 
-UPDATE student_info
-	SET name = 'madhur',email = 'madhur@gmail.com'
+desc Stu_info;
+
+
+UPDATE Stu_info
+	SET name = 'Lokesh',email = 'lokesh@gmail.com'
     WHERE id = 4;
 
-DELETE FROM student_info WHERE name= 'megha' OR rollno="82";
-DELETE FROM student_info WHERE name= 'megha' AND name="mihir";
+DELETE FROM Stu_info WHERE name= 'megha' OR rollno='82';
+DELETE FROM Stu_info WHERE name='Garry' AND rollno='22';
 
-UPDATE student_info
+UPDATE Stu_info
      SET name = 'sanket',email = 'sanket@gmail.com'
      WHERE id = 1 AND rollno= '81';
