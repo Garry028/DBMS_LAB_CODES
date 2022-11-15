e214dda629a5e1725f29b1a0226a9a9b007aa95e
Garry <gmisal2002@gmail.com>
2022-11-15 14:54:02 +0530

    changes

--- Assignment_2a_DBMS.sql
+++ Assignment_2a_DBMS.sql
@@ -1,7 +1,3 @@
-show databases;
-CREATE DATABASE ddl_commands;
-use ddl_commands;
-
 CREATE TABLE student_info(
      rollno INT PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
@@ -10,34 +6,36 @@ CREATE TABLE student_info(
 
 desc student_info;
 
-show tables;
-
 CREATE VIEW view_student_info as (
     select rollno from student_info
 );
 
 desc view_student_info;
 
+-- like this index is created
 CREATE INDEX ind_name ON student_info(name);
 
-SELECT * from student_info;
+-- like this we can delete index
+ALTER TABLE student_info
+DROP INDEX ind_name;
 
 -- sequence
  CREATE TABLE info(
-     id INT AUTO_INCREMENT PRIMARY KEY,
+     id INT PRIMARY KEY,
      rollno INT NOT NULL,
      name VARCHAR(255)
 );
 
-show tables;
 
-INSERT INTO info (rollno, name) VALUES ('081','mihir');
-INSERT INTO info (rollno, name) VALUES ('082','megha');
-INSERT INTO info (rollno, name) VALUES ('083','sanket');
-INSERT INTO student_info(rollno,name,email) VALUES("12","Garry","gmisal2002@gmail.com");
-INSERT INTO student_info(rollno,name,email) VALUES("19","Harry","Hmisal2002@gmail.com");
-INSERT INTO student_info(rollno,name,email) VALUES("22","Carry","Cmisal2002@gmail.com");
+INSERT INTO info (id,rollno, name) VALUES ('1','081','mihir');
+INSERT INTO info (id,rollno, name) VALUES ('2','082','megha');
+INSERT INTO info (id,rollno, name) VALUES ('3','083','sanket');
 
+
+-- synonym is replica of the StudentInfo table
+CREATE synonym StudentInfo for student;
+
+desc student;
 
 
 SELECT * FROM info;
