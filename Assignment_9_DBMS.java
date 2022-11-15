7ea7c3cd0d12e4fb00f1e139adc2db809a78f8ee
Garry <gmisal2002@gmail.com>
2022-11-15 19:20:59 +0530

    JDBC added

--- Assignment_9_DBMS.java
+++ Assignment_9_DBMS.java
@@ -1,24 +1,51 @@
-import  java.sql.*;
+import java.sql.*;
+import java.util.*;
+
 public class Assignment_9_DBMS {
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost: 3307/dml_commands","root", "your_password");
+            System.out.println("Connection Succesfully");
+            Statement sm = c.createStatement();
+            int ch;
+            do {
+                System.out.println("Enter Chice \n 1.Insert \n 2.Select \n3.Update \n 4.Delete \n 5.Exit");
+                ch=s.nextInt();
+                switch (ch) {
+                    case 1:
+                        String sql = "INSERT INTO customer VALUES(10,'Garry',5000)";
+                        sm.executeUpdate(sql);
+                        System.out.println("Record is Inserted");
+                        break;
 
-        System.out.println("Sql connection testing");
-
-        try {
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/sql_invoicing","root","password");
-            System.out.println("Connection Established");
-            Statement st= con.createStatement();
-            ResultSet rs=st.executeQuery("select * from clients where city='waco'");
-            System.out.println("Client_id \t name \t address \t city \t state \t phone");
-            System.out.println("______________________________________________________");
-            while (rs.next()){
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "+
-                        rs.getString(5)+"  "+rs.getString(6));
-            }
-            con.close();
+                    case 2:
+                        String sql1 = "select * from customer";
+                        ResultSet rs = sm.executeQuery(sql1);
+                        while (rs.next()) {
+                            String name1 = rs.getString("name");
+                            int salary = rs.getInt("salary");
+                            System.out.print("name " + name1);
+                            System.out.println(", salary: " + salary);
+                        }
+                        break;
+                    case 3:
+                        String sql2 = "update customer set name='Harish' where name='Garry' ";
+                        sm.executeUpdate(sql2);
+                        System.out.println("Record is updated");
+                        break;
+                    case 4:
+                        String sql3 = "delete from customer where name='Harish'";
+                        sm.executeUpdate(sql3);
+                        System.out.println("Record is deleted");
+                        break;
+                }
+            } while (ch < 5);
+            s.close();
         }
         catch(Exception e){
-            System.out.println("Connection Failed");
+            e.printStackTrace();
         }
     }
 }
