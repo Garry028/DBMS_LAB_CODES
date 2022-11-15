7f6d3856506b939e963ab0a64c82fd1b585d1a6c
Garry <gmisal2002@gmail.com>
2022-11-15 19:48:53 +0530

    MongoDB connectivity added

--- MongoDBConnectivity.java
+++ MongoDBConnectivity.java
@@ -1,41 +1,57 @@
 import com.mongodb.*;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+import java.util.*;
 
-public class MongoDBConnectivity {
+public class ConnectDB {
     public static void main(String[] args) {
-        try{
-            MongoClient db = new MongoClient("localhost",27017);
-            MongoCredential credential;
-            credential = MongoCredential.createCredential("Garry","Netflix","password".toCharArray());
-            System.out.println("Succesfully connected to mongoDB");
+        Scanner s = new Scanner(System.in);
 
-            MongoDatabase database=db.getDatabase("Netflix");
+        try {
+            MongoClient mongo = new MongoClient("localhost", 27017);
+            DB db = mongo.getDB("MY_DB");
+            DBCollection coll = db.getCollection("users");
+            int ch;
+            do {
+                System.out.println("Enter Chice \n 1.Insert \n 2.Display \n3.Update \n 4.Remove \n 5.Exit");
+                ch = s.nextInt();
+                switch (ch) {
+                    case 1:
+                        BasicDBObject d1 = new BasicDBObject("rno", "1").append("name", "Monika").append("age", "17");
+                        coll.insert(d1);
+                        System.out.println("Inserted Succesfully");
+                        break;
+                    case 2:
+                        DBCursor cursor = coll.find();
+                        while (cursor.hasNext()) {
+                            System.out.println(cursor.next());
+                        }
+                        System.out.println("Inserted Successfully");
 
-            MongoCollection collection=database.getCollection("series");
-            Document Witcher= new Document();
-            Document MoneyHeist=new Document();
+                        break;
+                    case 3:
+                        BasicDBObject query = new BasicDBObject();
+                        query.put("name", "Monika");
+                        BasicDBObject newDocument = new BasicDBObject();
+                        newDocument.put("name", "Ragini");
+                        BasicDBObject updateObj = new BasicDBObject();
+                        updateObj.put("$set", newDocument);
+                        coll.update(query, updateObj);
+                        System.out.println("Updated Successfully");
 
-            Witcher.append("_id",1)
-                    .append("Series","The Witcher")
-                    .append("Genre","Sci-fi")
-                    .append("Seasons","3")
-                    .append("Actor","Henry Cavil" );
-            MoneyHeist.append("_id",2)
-                    .append("Series","La casa de papel")
-                    .append("Genre","Crime")
-                    .append("Seasons","5")
-                    .append("Actor","Professor" );
-
-            collection.insertOne(Witcher);
-            collection.insertOne(MoneyHeist);
-            System.out.println("Documents inserted succesfully");
-        }
-        catch(Exception e){
+                        break;
+                    case 4:
+                        BasicDBObject searchQuery = new BasicDBObject("name", "Monika");
+                        coll.remove(searchQuery);
+                        System.out.println("Removed Successfully");
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;
+                }
+            } while (ch < 4);
+        } catch (Exception e) {
             System.out.println("Connection Failed");
             System.out.println(e);
-        }
-    }
+        }
+    }
 
 }
