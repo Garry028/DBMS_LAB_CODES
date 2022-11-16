00d31742814d686c21e6346e7aa816175d364671
Garry <gmisal2002@gmail.com>
2022-11-16 21:13:38 +0530

    Added table for area and radius

--- Assignment_5_DBMS.sql
+++ Assignment_5_DBMS.sql
@@ -1,3 +1,5 @@
+CREATE TABLE Circle(radius NUMBER,area NUMBER);
+
 DECLARE
 	radius_var NUMBER;
 	area_var NUMBER;
@@ -6,5 +8,10 @@ BEGIN
 	FOR radius_var IN 5 .. 9 LOOP
 		area_var := pi*radius_var*radius_var;
 		dbms_output.put_line(area_var);
+		INSERT INTO Circle VALUES (radius_var,area_var);
 	END LOOP;
+
 END;
+/
+
+SELECT * FROM Circle;
